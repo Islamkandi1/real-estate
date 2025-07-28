c5587596e7493279e485f7693134117ff4423a0b
Islamkandi1 <eslamkandil097@gmail.com>
2025-07-28 15:17:15 +0300

    reacr-router scroll handle

--- src/layout/Layout.jsx
+++ src/layout/Layout.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
 import Navbar from './../navbar/Navbar';
-import { Outlet } from 'react-router-dom';
+import { Outlet, ScrollRestoration } from 'react-router-dom';
 import Footer from '../footer/Footer';
 import TopBtn from './../to-btn/TopBtn';
 
 const Layout = () => {
   return (
     <>
+    <ScrollRestoration/>
       <TopBtn/>
       <Navbar/>
       <Outlet/>
